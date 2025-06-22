d18d028a60c9b6e863c8ccf481c48dbf9abe0c4d
UgbeGodwinIkpe <ugbegodwin7963@gmail.com>
2025-06-22 07:58:51 +0100

    payment verifaction

--- controllers/payment.js
+++ controllers/payment.js
@@ -101,11 +101,11 @@ exports.VerifyPayment= async (req, res) => {
               >
             </head>
             <body style="font-family: Arial; padding: 5px; justify-content:center">
-              <div style="background:blue; color:white; margin: 30px auto 0px auto; padding:15px; width:fit-content; text-align:center;">
-                <h1>Success</h1>
-                <h2>Your payment was successful and has been confirmed: <del>N</del>${response.data.data.amount/100}</h2>
+              <div style="background:green; color:white; margin: 30px auto 0px auto; padding:15px; width:fit-content; text-align:center;">
+                <h1>Payment Successful</h1>
+                <h2>Your payment has been confirmed: <del>N</del>${response.data.data.amount/100}</h2>
                 <p>Thank you for trusting ziman</p>
-                <a href="https://ziman.com.ng" style="background:green; color:white; padding:10px; text-decoration:none;">Return Back to App</a>
+                <a href="https://ziman.com.ng" style="background:white; color:green; padding:10px; text-decoration:none;">Return Back to App</a>
               </div>
 
               
