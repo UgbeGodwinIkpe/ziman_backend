7b6bf8f71d04266d0a65fda08ed58f8b59e114e7
UgbeGodwinIkpe <ugbegodwin7963@gmail.com>
2025-08-03 21:08:31 +0100

    reset passwrd updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -95,7 +95,7 @@ exports.register = async (req, res) => {
     console.log(req.body)
     const { username, email, password, fullName, role } = req.body;
     // let emailAdd=email.toLowerCase()
-    const existing = await User.findOne({ emailAdd });
+    const existing = await User.findOne({ email });
     if (existing) return res.status(400).json({ message: 'Email already in use' });
     const hashed = await bcrypt.hash(password, 10);
     const vcode= await sendVerificationEmail(email, username)
