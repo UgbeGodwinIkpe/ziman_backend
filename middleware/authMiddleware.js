4f17a99dc60c1d2f0bbd68a1397417aec4118a34
UgbeGodwinIkpe <ugbegodwin7963@gmail.com>
2025-08-17 19:50:36 +0100

    updates on pickup request

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -35,7 +35,7 @@ module.exports = async (req, res, next) => {
       next();
     }
   } catch (err) {
-    res.status(401).json({ message: 'Token invalid' });
+    res.status(401).json({ message: 'Token invalid', errr:err });
   }
 };
 
