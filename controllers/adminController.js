74c34f2f0efe60cabf5918a8443333a63c3f7c74
UgbeGodwinIkpe <ugbegodwin7963@gmail.com>
2025-08-28 21:06:30 +0100

    new changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -152,7 +152,7 @@ exports.getMerchantSalesPayout=async (req, res) => {
   orders.forEach(order => {
     order.restaurant.bankAccount = bankAccounts.find(account => account.restaurant.toString() === order.restaurant._id.toString()) || "Acct Details";
   });
-  console.log({pay:orders.restaurant})
+  console.log({pay:orders[0].restaurant})
   res.status(200).json(orders);
   
   // const orders1 = await Order.find({paid:true }).sort({ createdAt: -1 })
