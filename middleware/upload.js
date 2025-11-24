077af33a420489b3bd4e2f5833645ca353eef7b0
UgbeGodwinIkpe <ugbegodwin7963@gmail.com>
2025-11-25 00:31:17 +0100

    checking registration

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,11 +1,20 @@
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "ziman_app_images",   // your upload folder on Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png"],
   },
 });
+// const storage = multer.diskStorage({
+//   destination: "uploads/",
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
 
 const upload = multer({ storage });
 module.exports = upload;
